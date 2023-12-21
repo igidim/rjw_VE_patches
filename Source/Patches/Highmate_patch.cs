d4aacdbd469d948f349a0b17fad563841a0b250b
igidim <igidim@proton.me>
2023-12-21 12:38:47 +0200

    upload

---
+++ Source/Patches/Highmate_patch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VanillaRacesExpandedHighmate;
+using Verse;
+
+namespace RJW_VE_patches.Patches
+{
+    [HarmonyPatch]
+    public static class Highmate_patch
+    {
+        [HarmonyPatch(typeof(rjw.AfterSexUtility), "FinalizeThought")]
+        public static void Postfix(ThoughtDef thoughtgain, Pawn pawn) 
+        {
+            Need_Lovin need = pawn?.needs?.TryGetNeed<Need_Lovin>();
+            if (need != null)
+            {
+                need.CurLevel = 1f;
+            }
+        }
+    }
+}
---
+++ Source/main.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using VanillaRacesExpandedHighmate;
+using Verse;
+
+namespace RJW_VE_patches
+{
+    [StaticConstructorOnStartup]
+    public class RJW_VE_main
+    {
+        static RJW_VE_main()
+        {
+            var harmony = new Harmony("com.rjw.ve_highmate_patch");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
